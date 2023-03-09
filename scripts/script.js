2789b78fa9549b70d712ef6d4e805700eec469e9
Daniel Garrido <daniel.garrido@epn.edu.ec>
2023-03-09 01:29:48 -0500

    listo el tab index

--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,40 @@
 let boton = document.getElementById('consulta');
 let respuesta = document.getElementById('respuesta')
 let editText = document.getElementById('id_number')
+let columna = document.getElementsByTagName('th')
+let respuestaCol = document.getElementsByTagName('td')
+const regexNumber = new RegExp('[1-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
+let elementosBarraBottom = document.getElementsByClassName('barra_bottom')[0]
+let elementosABottom = elementosBarraBottom.getElementsByTagName('a')
+let iconosBarra = elementosBarraBottom.getElementsByTagName('i')
 
+
+for(var i=0;i<columna.length;i++){
+    columna[i].setAttribute("tabindex","1")
+}
+
+for(var i=0;i<respuestaCol.length;i++){
+    respuestaCol[i].setAttribute("tabindex","1")
+}
+
+for(var i=0;i<elementosABottom.length;i++){
+    elementosABottom[i].setAttribute("tabindex","1")
+}
+for(var i=0;i<iconosBarra.length;i++){
+    iconosBarra[i].setAttribute("tabindex","1")
+}
 
 boton.addEventListener('click',enviarRespuesta);
-if (editText.val() === '') {
-    editText.attr("aria-invalid", "true");
-    editText.addClass('failed');
-} 
+
+
 
 function enviarRespuesta(){
-   respuesta.style.display = "";
+   if (regexNumber.test(parseInt(editText.value))) {
+    respuesta.style.display = "";
+    editText.setAttribute("aria-invalid", "false");
+    }else{
+        editText.setAttribute("aria-invalid", "true");
+    editText.classList.add('failed');
+        console.log('sdfsadf')
+    }
 }
